bd89026dab3f70e50aaec86f1fb2438779f3c40b
nowayrm <your.email@example.com>
2026-01-02 15:50:35 +0200

    some updates

--- bot/start_bot.sh
+++ bot/start_bot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-source /root/Bot/myenv/bin/activate
-nohup python3 /root/Bot/main.py > /dev/null 2>&1 &
+source /root/AllYouNeed/bot/myenv/bin/activate
+nohup python3 /root/AllYouNeed/bot/main.py > /dev/null 2>&1 &
+echo "Bot started"
